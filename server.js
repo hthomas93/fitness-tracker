d264492d172a2ed34717a4f4c0181b4e2508adbc
Harrison Thomas <hthomas146@utexas.edu>
2020-02-13 18:28:04 -0600

    Connected the database to the server with a .env file

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
+// Requires express to set up the server
 const express = require("express");
+// Requires mongoose to connect to the database
 const mongoose = require("mongoose");
+// Requires logger
 const logger = require("morgan");
+
+
 
 // Initializes port
 const PORT = process.env.PORT || 3030;
@@ -11,12 +16,17 @@ const db = require("./models")
 
 app.use(logger("dev"));
 
+// Tells the server what kind of data to use
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Connect to the database
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
 // Looks inside the public folder and displays any static files, such as HTML documents
 app.use(express.static("public"));
 
+// Creates 
 db.Workout.create({ name: "Fitness Tracker" })
     .then(dbWorkout => {
         console.log(dbWorkout);
